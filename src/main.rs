ed6b3823c28c593c2a1f3cd7943d02ea27d294a9
Kanishk Sakarwar <krishshlokh@gmail.com>
2024-11-15 13:19:36 +0530

    Added Rust Code and cargo files

---
+++ src/main.rs
@@ -0,0 +1,32 @@
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+use colored::*;
+
+fn main(){
+    let secret_number = rand::thread_rng().gen_range(1,10);
+    //println!("{} is secret number",secret_number);
+    
+    loop{
+        let mut guess = String::new();
+        println!("Guess a number:");
+    
+        io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read");
+
+        let guess:u32 = match guess.trim().parse(){
+            Ok(num)=>num,
+            Err(_)=>continue,
+        };
+
+        match guess.cmp(&secret_number){
+            Ordering::Less => println!("{}","Too Small".red()),
+            Ordering::Greater => println!("{}","Too Big".red()),
+            Ordering::Equal => {
+                println!("{}","Correct".green());
+                break;
+            }
+        }
+    }
+}
